18dc1b74ff03b90a15aa775eacea5137769f7554
Crodri9 <carolinemrodrigues9@gmail.com>
2023-06-13 14:37:04 -0400

    Confirm if input is narcissistic number or not

--- Assignment2_CR.R
+++ Assignment2_CR.R
@@ -1,14 +1,15 @@
 # Script to let user know whether input is Armstrong number
 
+# Prompt user to enter three-digit positive number
 answer <- (readline(prompt = "Please enter a three digit positive number: "))
 
-# Check to see if input is numeric. 
+# Check to see if input is numeric, if not, print error message and quit
 if (is.na(as.integer(answer))) {
   print ("Error: Input not numeric") 
   quit(save = "no")
   }
 
-# Convert input into integer 
+# Convert input into integer
 answer <- as.integer(answer)
 
 # Check if number is 3 digits
@@ -17,8 +18,12 @@ if (answer < 100 || answer > 999) {
   quit (save = "no")
 }
 
-digits <- as.numeric(unlist(strsplit(as.character(answer), ""))) %>%
-narcissistic_1 <- sum(digits^3)
-narcissistic <- answer == narcissistic_1
+# Use strsplit to split elements of the vector into substrongs. 
+digits <- as.numeric(unlist(strsplit(as.character(answer), "")))
+# Sum the cube of each sub string
+narcissistic <- sum(digits^3)
+narcissistic1 <- answer == narcissistic
 
-
+if (narcissistic1) {print(paste(answer, "is a narcissistic number"))} else 
+{ print(paste(answer, "is not a narcissistic number"))
+}
